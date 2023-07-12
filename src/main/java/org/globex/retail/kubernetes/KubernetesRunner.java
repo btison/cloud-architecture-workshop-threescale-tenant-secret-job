6926a37d18462c1b9cb05df3c3625e1a0e70baeb
Bernard Tison <bernard.tison@gmail.com>
2023-07-12 19:34:03 +0200

    Secret might not yet exist when job is started: woit until secret is created

--- src/main/java/org/globex/retail/kubernetes/KubernetesRunner.java
+++ src/main/java/org/globex/retail/kubernetes/KubernetesRunner.java
@@ -3,11 +3,14 @@ package org.globex.retail.kubernetes;
 import io.fabric8.kubernetes.api.model.Secret;
 import io.fabric8.kubernetes.api.model.SecretBuilder;
 import io.fabric8.kubernetes.client.KubernetesClient;
+import io.fabric8.kubernetes.client.dsl.Resource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
 import java.util.regex.Pattern;
 
 @ApplicationScoped
@@ -37,14 +40,19 @@ public class KubernetesRunner {
 
         String threescaleTenantSecretCopyName = System.getenv().getOrDefault("THREESCALE_SECRET_COPY", "3scale-tenant-secret");
 
-        Secret threescaleTenantSecret = client.secrets().inNamespace(threescaleNamespace).withName(threescaleTenantSecretName).get();
-        if (threescaleTenantSecret == null) {
-            LOGGER.error("Secret " + threescaleTenantSecretName + " not found in namespace " + threescaleNamespace + ". Exiting");
+        String maxTimeToWaitStr = System.getenv().getOrDefault("MAX_TIME_TO_WAIT_MS", "600000");
+        long maxTimeToWait = Long.parseLong(maxTimeToWaitStr);
+
+        Resource<Secret> threescaleTenantSecret = client.secrets().inNamespace(threescaleNamespace).withName(threescaleTenantSecretName);
+        try {
+            threescaleTenantSecret.waitUntilCondition(Objects::nonNull, maxTimeToWait, TimeUnit.MILLISECONDS);
+        } catch (Exception e) {
+            LOGGER.error("Secret " + threescaleTenantSecretName + " in namespace " + threescaleNamespace + " is not ready after " + maxTimeToWaitStr + " milliseconds. Exiting...", e);
             return -1;
         }
 
         Secret newSecret = new SecretBuilder().withNewMetadata().withName(threescaleTenantSecretCopyName).endMetadata()
-                .addToData(threescaleTenantSecret.getData()).build();
+                .addToData(threescaleTenantSecret.get().getData()).build();
         client.secrets().inNamespace(namespace).resource(newSecret).createOrReplace();
 
         LOGGER.info("Secret " + threescaleTenantSecretCopyName + " created in namespace " + namespace + ".");
